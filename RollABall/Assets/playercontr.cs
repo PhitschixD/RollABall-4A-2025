d5a96b9c3358bed0ed12a4fe3b602661cebf4f92
PhitschixD <philip.holauf@gmail.com>
2025-03-04 12:37:27 +0100

    Text.gegner.viel gemacht

--- RollABall/Assets/playercontr.cs
+++ RollABall/Assets/playercontr.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
+using TMPro;
 
 public class playercontr : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed;
+    public Vector3 ForceVector;
+
+    public TextMeshProUGUI scoretext;
+    public TextMeshProUGUI gewonnentext;
+    
 
     private float movementX;
     private float movementY;
+
+    private int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        gewonnentext.gameObject.SetActive(false);
     }
 
     private void OnMove(InputValue movementvalue)
@@ -26,7 +35,30 @@ public class playercontr : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space!");
+            rb.AddForce(ForceVector, ForceMode.Impulse);
+        }
+
         Vector3 movement = new Vector3(movementX,0.0f, movementY );
         rb.AddForce(movement* speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!other.gameObject.CompareTag("pickUp"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            scoretext.text =  ("Score: " +  score);
+        }
+
+        if(score == 14) 
+        {
+            gewonnentext.gameObject.SetActive(true);
+            gewonnentext.text = ("sie haben gewonnen.");
+        }
+        
     }
 }
